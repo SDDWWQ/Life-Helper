0ef76d6b050f6f647a52e5df44fb0e79a5cbcce6
SDD <1058088225@qq.com>
2016-07-31 14:42:11 +0800

    首页天气信息显示

--- LifeHelper/LifeHelper/HomeViewController.m
+++ LifeHelper/LifeHelper/HomeViewController.m
@@ -27,17 +27,85 @@
     [self request: httpUrl withHttpArg: httpArg];//加载天气信息
     UIView *weatherView=[[UIView alloc]init];
     weatherView.frame=CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height*0.25);
+    //天气整个大button
     UIButton *weatherBtn=[[UIButton alloc]init];
     [weatherBtn setBackgroundImage:[UIImage imageNamed:@"weather_background"] forState:UIControlStateNormal];
     weatherBtn.frame=weatherView.frame;
     [weatherView addSubview:weatherBtn];
-    UILabel *cityLabel=[[UILabel alloc]initWithFrame:CGRectMake(10, 10, self.view.frame.size.width, 20)];
+    //显示城市Label
+    UILabel *cityLabel=[[UILabel alloc]initWithFrame:CGRectMake(10, 5, self.view.frame.size.width*0.5, 15)];
+    
     NSString *city=[NSString stringWithFormat:@"%@  %@  %@", self.weather.city,self.weather.date,self.weather.temp];
-    cityLabel.text=city;
+    cityLabel.text=self.weather.city;
     [weatherBtn addSubview:cityLabel];
+    
+    //更新时间Label
+    UILabel *timeLabel=[[UILabel alloc]initWithFrame:CGRectMake(10, 25, self.view.frame.size.width*0.5, 10)];
+    NSDate *nowDate=[NSDate date];//格林威治时间
+    NSDateFormatter *dateFormatter=[[NSDateFormatter alloc]init];
+    dateFormatter.dateFormat=@"yy-MM-dd HH:mm";
+    //NSString *nowDateStr=[dateFormatter stringFromDate:nowDate];//格式化之后就是本地时间
+    //NSLog(@"%@",nowDateStr);
+    NSString *weatherDateStr=[NSString stringWithFormat:@"%@ %@",self.weather.date,self.weather.time];
+    NSDate *weatherDate=[dateFormatter dateFromString:weatherDateStr];
+    //NSLog(@"%@",weatherDate);
+    //创建一个日历对象
+    NSCalendar *calendar=[NSCalendar currentCalendar];
+    //比较时间
+    int unit=NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay|NSCalendarUnitHour|NSCalendarUnitMinute;
+    NSDateComponents *compare=[calendar components:unit fromDate:weatherDate toDate:nowDate options:0];
+    NSString *timeStr;
+    if (compare.year!=0) {
+        timeStr=[NSString stringWithFormat:@"%ld年前更新",compare.year];
+    }else if (compare.month){
+        timeStr=[NSString stringWithFormat:@"%ld个月前更新",compare.month];
+    }else if(compare.hour!=0) {
+        timeStr=[NSString stringWithFormat:@"%ld小时前更新",compare.hour];
+    }else if(compare.minute){
+        timeStr=[NSString stringWithFormat:@"%ld分钟前更新",compare.minute];
+    }
+    else if(compare.minute==0){
+        timeStr=@"刚刚更新";
+    }
+    timeLabel.text=timeStr;
+    timeLabel.font=[UIFont systemFontOfSize:10];
+    [weatherBtn addSubview:timeLabel];
+    
+    //温度Label
+     UILabel *tempratureLabel=[[UILabel alloc]initWithFrame:CGRectMake(10, 40, self.view.frame.size.width*0.5, 15)];
+    tempratureLabel.text=[NSString stringWithFormat:@"%@°C",self.weather.temp];
+    [weatherBtn addSubview:tempratureLabel];
+    
+    //风向风力Label
+    UILabel *windDirectionLabel=[[UILabel alloc]initWithFrame:CGRectMake(10, 60, self.view.frame.size.width*0.5, 15)];
+    windDirectionLabel.text=[NSString stringWithFormat:@"%@",self.weather.WD];
+    //windDirectionLabel.numberOfLines=0 ;
+    [weatherBtn addSubview:windDirectionLabel];
+    //风力Label
+    UILabel *windStrengthLabel=[[UILabel alloc]initWithFrame:CGRectMake(10, 80, self.view.frame.size.width*0.5, 15)];
+    windStrengthLabel.text=[NSString stringWithFormat:@"%@",self.self.weather.WS];
+    //windStrengthLabel.numberOfLines=0 ;
+    [weatherBtn addSubview:windStrengthLabel];
+    
+    //天气图片和Label
+    NSString *weather=self.weather.weather;
+    NSString *path=[[NSBundle mainBundle]pathForResource:@"WeatherImage" ofType:@"plist"];
+    NSDictionary *weatherImgDict=[NSDictionary dictionaryWithContentsOfFile:path];
+    UIImageView *weatherImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:weatherImgDict[weather]]];//根据天气获取对应的天气图片
+    weatherImageView.frame=CGRectMake(self.view.frame.size.width*0.5, 10, self.view.frame.size.width*0.5, self.view.frame.size.height*0.2);
+    [weatherBtn addSubview:weatherImageView];
+    
+    UILabel *weatherLabel=[[UILabel alloc]initWithFrame:CGRectMake(self.view.frame.size.width*0.7, self.view.frame.size.width*0.2+10+5, self.view.frame.size.width*0.5-10, 15)];
+    weatherLabel.text=self.weather.weather;
+    [weatherBtn addSubview:weatherLabel];
+
+
     [self.homeScrollView addSubview:weatherView];
     
+    
+    
     [weatherBtn addTarget:self action:@selector(jump2WeatherDetailView) forControlEvents:UIControlEventTouchUpInside];
+    
 
 
     
